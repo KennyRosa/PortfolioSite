ceed5058efe4076f04e0b40303284be7339b4b94
Kenny Rosa <112502866+KennyRosa@users.noreply.github.com>
2023-02-05 19:52:44 -0500

    Update CallToAction.jsx

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -5,11 +5,11 @@ import "./header.css";
 function CallToAction() {
   return (
     <div className="cta">
+    <a href={resume} download className="btn">
+        Resume
+      </a>
       <a href="#contact" className="btn btn-primary">
         Let's Talk
-      </a>
-      <a href={resume} download className="btn">
-        Resume
       </a>
     </div>
   );
